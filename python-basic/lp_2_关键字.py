02e8f593342896d124eaa4c7c1282f908cf368df
Luop-code <1656483310@qq.com>
2021-10-31 21:59:21 +0800

    新增

---
+++ python-basic/lp_2_关键字.py
@@ -0,0 +1,4 @@
+# CDUT 罗澎
+# 开发时间：2021/11/22 22:07
+import keyword
+print (keyword.kwlist)
---
+++ python-basic/lp_4_loop.py
@@ -0,0 +1,8 @@
+# CDUT 罗澎
+# 开发时间：2021/11/23 14:36
+
+
+i = 0
+while i < 5:
+    print('hello world')
+    i = i + 1
